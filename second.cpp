b50394166b5922572f9e912e928846fbd8a6e7fd
Soumajit Chakraborty <soumajitchakraborty07@gmail.com>
2026-09-16 18:49:36 +0530

    Update second.cpp

--- second.cpp
+++ second.cpp
@@ -6,14 +6,6 @@ public:
         vector<vector<int>> ans;
         for (int i = 1; i < intervals.size(); ++i) {
             if (ed < intervals[i][0]) {
-                aclass Solution {
-public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
-        int st = intervals[0][0], ed = intervals[0][1];
-        vector<vector<int>> ans;
-        for (int i = 1; i < intervals.size(); ++i) {
-            if (ed < intervals[i][0]) {
                 ans.push_back({st, ed});
                 st = intervals[i][0];
                 ed = intervals[i][1];
@@ -21,23 +13,6 @@ public:
                 ed = max(ed, intervals[i][1]);
             }
         }
-        ans.push_back({st, ed});
-        return ans;
-    }
-}; ans;
-        for (int i = 1; i < intervals.size(); ++i) {
-            if (ed < intervals[i][0]) {
-                ans.push_back({st, ed});
-                st = intervals[i][0];
-                ed = intervals[i][1];
-            } else {
-                ed = max(ed, intervals[i][1]);
-            }
-        }
-        ans.push_back({st, ed});
-        return ans;
-    }
-};
         ans.push_back({st, ed});
         return ans;
     }
