e045c19cf9c1baf91e8c8cb479f6520c7f24ccba
Soumajit Chakraborty <soumajitchakraborty07@gmail.com>
2026-09-18 20:09:42 +0530

    Remove duplicate deleteDuplicates method
    Removed duplicate implementation of deleteDuplicates method.

--- third.cpp
+++ third.cpp
@@ -1,5 +1,3 @@
-
-clas
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
@@ -9,36 +7,6 @@ public:
         while (cur) {
             while (cur->next && cur->next->val == cur->val) {
                 cur = cur->next;
-            }
-            if (pre->next == cur) {
-                pre = cur;
-            } else {
-                pre->next = cur->next;
-            }
-            cur = cur->next;
-        }
-        return dummy->next;
-    }
-};
-public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        ListNode* dummy = new ListNode(0, head);
-        ListNode* pre = dummy;
-        ListNode* cur = head;
-        while (cur) {
-            while (cur->next && cur->next->val == cur->val) {
-                cur = cur->next;
-            }
-            if (pre->next == cur) {
-                pre = cur;
-            } else {
-                pre->next = cur->next;
-            }
-            cur = cur->next;
-        }
-        return dummy->next;
-    }
-};
             }
             if (pre->next == cur) {
                 pre = cur;
