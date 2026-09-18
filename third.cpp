9f4b9d3d36e60e79ad0890fdc56134bfd4b44157
Soumajit Chakraborty <soumajitchakraborty07@gmail.com>
2026-09-18 20:09:26 +0530

    Update third.cpp

--- third.cpp
+++ third.cpp
@@ -1,4 +1,5 @@
 
+clas
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
@@ -8,6 +9,36 @@ public:
         while (cur) {
             while (cur->next && cur->next->val == cur->val) {
                 cur = cur->next;
+            }
+            if (pre->next == cur) {
+                pre = cur;
+            } else {
+                pre->next = cur->next;
+            }
+            cur = cur->next;
+        }
+        return dummy->next;
+    }
+};
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* pre = dummy;
+        ListNode* cur = head;
+        while (cur) {
+            while (cur->next && cur->next->val == cur->val) {
+                cur = cur->next;
+            }
+            if (pre->next == cur) {
+                pre = cur;
+            } else {
+                pre->next = cur->next;
+            }
+            cur = cur->next;
+        }
+        return dummy->next;
+    }
+};
             }
             if (pre->next == cur) {
                 pre = cur;
