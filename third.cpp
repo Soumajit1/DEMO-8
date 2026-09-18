c5f2f1e466551299f4a9a9900f05aeb57003b47b
Soumajit Chakraborty <soumajitchakraborty07@gmail.com>
2026-09-18 20:10:25 +0530

    Update third.cpp

--- third.cpp
+++ third.cpp
@@ -18,24 +18,3 @@ public:
         return dummy->next;
     }
 };
-
-class Solution {
-public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        ListNode* dummy = new ListNode(0, head);
-        ListNode* pre = dummy;
-        ListNode* cur = head;
-        while (cur) {
-            while (cur->next && cur->next->val == cur->val) {
-                cur = cur->next;
-            }
-            if (pre->next == cur) {
-                pre = cur;
-            } else {
-                pre->next = cur->next;
-            }
-            cur = cur->next;
-        }
-        return dummy->next;
-    }
-};
