3b0d8f183f86725634347cf46634d3154fcee042
Soumajit Chakraborty <soumajitchakraborty07@gmail.com>
2026-09-21 22:31:09 +0530

    Create seven.cpp

---
+++ seven.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans;
+        vector<int> t;
+        function<void(int, int)> dfs = [&](int i, int s) {
+            if (s == 0) {
+                ans.emplace_back(t);
+                return;
+            }
+            if (s < candidates[i]) {
+          
+    }
+};
